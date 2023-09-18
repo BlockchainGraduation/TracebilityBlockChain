27e65d80fbc50db8a3c600eace17181fc43bdc8a
Duy Luan <nvdluan@gmail.com>
2023-09-19 00:56:37 +0700

    add contract MarketPlace

---
+++ contracts/MarketPlace.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MarketPlace{
+    enum typeProduct{Seedling, Fruit}
+    struct Product{
+        string productId;
+        uint quantity;
+        address productOwner;
+        typeProduct productType;
+    }
+    struct Transaction{
+        string transactionId;
+        string productId;
+        uint quantity;
+        address buyer;
+    }
+    mapping (address => Product[]) public listProduct;
+    mapping (address=>uint) countProdct;
+    mapping (address => Transaction[]) public historyTransaction;
+
+    function listing_product(string memory productId, uint quantity, address companyAddress, typeProduct productType) public {
+        listProduct[companyAddress].push(Product(productId, quantity, companyAddress, productType));
+    }
+    function buyProduct(string memory transactionId, string memory productId, uint quantity) public {
+        historyTransaction[msg.sender].push(Transaction(transactionId, productId, quantity, msg.sender)); 
+    }
+    function unlisting(string memory productId, address companyAddress) public {
+        Product[] storage products = listProduct[companyAddress];
+        uint productIndex = countProdct[companyAddress];
+
+        Product[] memory newListProduct = new Product[](productIndex);
+
+        uint newProductIndex = 0;
+        for (uint i = 0; i < productIndex; i++) {
+            if (keccak256(abi.encodePacked(products[i].productId)) != keccak256(abi.encodePacked(productId))) {
+                newListProduct[newProductIndex] = products[i];
+                newProductIndex++;
+            }
+        }
+
+        listProduct[companyAddress] = newListProduct;
+        countProdct[companyAddress] = newProductIndex;
+    }
+
+
+}
--- contracts/SeedCompany.sol
+++ contracts/SeedCompany.sol
@@ -1,22 +1,64 @@
 // SPDX-License-Identifier: MIT
+// import "@openzeppelin/contracts/access/Ownable.sol";
 pragma solidity ^0.8.0;
+interface ISeedCompany {
+    function add_seed(string memory _id, uint128 _quantity) external;
+}
+library SupplyChainLib{
+    enum typeEntities {Fammer, SeedCompany, Manufacturer}
+}
+contract SeedCompany is ISeedCompany{
 
-contract SeedCompany{
     struct Company_info{
         string id;
         address owner;
+        SupplyChainLib.typeEntities companyType;
     }
-
     struct Categories{
         string id;
         uint128 quantity;
+        uint32 created_at;
     }
+    string[] public categories_id;
+    Company_info public company_info;
+    mapping(string => Categories) public categories;
+    constructor(string memory id, address owner, SupplyChainLib.typeEntities companyType){
+        company_info = Company_info(id, owner, companyType);
+    }
+    function add_seed(string memory _id, uint128 _quantity) public {
+        categories[_id] = Categories(_id,_quantity, uint32(block.timestamp));
+        categories_id.push(_id);
+    }
+}
 
-    Company_info company_info;
+
+
+contract SystemFactory {
     
-
-    function get_info() public view returns(string memory id, address owner){
-        return (company_info.id, company_info.owner);
+    // mapping (SupplyChainLib.typeEntities => address[]) public factory;
+    address[] public factory;
+    uint index = 0;
+    SeedCompany[] seedCompanys;
+    function deploy(string calldata _id, uint8 entity) public {
+        SupplyChainLib.typeEntities temp = hasTypeEntity(entity);
+        SeedCompany seedcompany = new SeedCompany(_id, msg.sender, temp);
+        factory.push(address(seedcompany));
+        seedCompanys.push(seedcompany);
     }
+    function hasTypeEntity(uint8 entity) public pure returns(SupplyChainLib.typeEntities result) {
+       
+        if (entity == 1) {
+            result = SupplyChainLib.typeEntities.SeedCompany;
+        } 
+        else if (entity == 2) {
+            result = SupplyChainLib.typeEntities.Fammer;
+        } 
+        else if (entity == 3){
+            result = SupplyChainLib.typeEntities.Manufacturer;
+        }
+        else {
+            revert("entity invalid");
+        }
 
+    }
 }
